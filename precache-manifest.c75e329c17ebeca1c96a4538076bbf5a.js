75a47a8a11870a8defcf53a2750f1d92697550a5
Preston Chandler <pchandler858@gmail.com>
2019-10-10 22:49:12 -0700

    Updates

---
+++ precache-manifest.c75e329c17ebeca1c96a4538076bbf5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324b2d72f3117a98a27ba6a685b16670",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "568cdd399d006afed1dc",
+    "url": "/monsters-rolodex/static/css/main.fa1fb690.chunk.css"
+  },
+  {
+    "revision": "7650c827e7a662578045",
+    "url": "/monsters-rolodex/static/js/2.e6c976b8.chunk.js"
+  },
+  {
+    "revision": "568cdd399d006afed1dc",
+    "url": "/monsters-rolodex/static/js/main.1536e8b2.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
